937d8c5245bf394aea6b6777ee9d59126955952e
MattSainz <Matthias.Sainz@colorado.edu>
2014-11-25 12:59:05 -0700

    Lab12

---
+++ adapter.py
@@ -0,0 +1,21 @@
+import facade
+
+# The Adaptor class should be implemented so that it has two methods that can be called by the running code.
+# As you can see, it should contain a start function to start the computer, and a getComputerInfo function to get the information of the CPU, memory and hard disk. 
+# You can only use the Computer class from the facade.py you implemented. Do not use the classes in parts.py 
+class Adapter:
+  def __init__(self): 
+    self.a_cpu = facade.Computer()
+
+  def start(self):
+    self.a_cpu.startComputer()
+
+  def getComputerInfo(self):
+    self.a_cpu.printCPUInfo()
+    self.a_cpu.printMemInfo()
+    self.a_cpu.printHDInfo()
+
+# The running part. You should not modify this part.
+newcomputer=Adapter()
+newcomputer.start()
+newcomputer.getComputerInfo()
